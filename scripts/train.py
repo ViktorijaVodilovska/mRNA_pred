6bd86703a53374b2d582df4b1046e1f0ee08d70f
ViktorijaVodilovska <viktorija.vodilovska@gmail.com>
2022-12-25 23:04:30 +0100

    added saving model in train

--- scripts/train.py
+++ scripts/train.py
@@ -1,17 +1,14 @@
-import os
 import wandb
 import torch
-from pathlib import Path
-from config import TrainConfig as train_settings, BaseSettings
+from configs.predictor_config import PredictorConfig as train_settings
 from scripts.evaluate import test_model
 
 
-def train_model(model, train_loader, val_loader, epochs, target_labels, save_to: Path = None, loss_type: str = 'mse', learning_rate: float = 0.01, hetero=False, log=True):
+def train_model(model, train_loader, val_loader, epochs, target_labels, loss_type: str = 'mse', learning_rate: float = 0.01, hetero=False, log=True, save_to=None):
     criterion = train_settings.loss_function[loss_type]()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=5e-4)
 
-    if log:
-        wandb.init(project=BaseSettings.PROJECT_NAME)
+    last = None
     
     for epoch in range(epochs):
         
@@ -25,19 +22,24 @@ def train_model(model, train_loader, val_loader, epochs, target_labels, save_to:
             loss.backward()  # Derive gradients.
             optimizer.step()  # Update parameters based on gradients.
             losses.append(loss.detach().numpy())
-        
+
         res = test_model(val_loader, model, target_labels, hetero=hetero)
         res['epoch'] = epoch
         epoch_loss = sum(losses)/len(losses) if len(losses)>0 else 0
         print(epoch_loss)
         res['epoch_loss'] = epoch_loss
         print(res)
+        
         if log:
             wandb.log(res)
-        
-        # check before saving
+
         if save_to != None:
-            torch.save(model.state_dict(), save_to)
+            # TODO: log as artifact
+            # save only better epochs
+            if last == None or last > res['mcrmse']:
+                torch.save(model.state_dict(), save_to)
+
+        last = res['mcrmse']
         
-    return res
+    return res, model
     
